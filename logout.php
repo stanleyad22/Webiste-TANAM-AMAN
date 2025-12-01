648ecb2c8210e1c6e23e8609c48032ef3d7e2f99
Stanley AD <312010010@student.machung.ac.id>
2025-12-01 18:09:03 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
 
-// Hapus semua variabel session
 $_SESSION = array();
 
-// Hancurkan session
 session_destroy();
 
-// Redirect kembali ke halaman utama
-// GANTI 'index.php' dengan nama file utama Anda jika berbeda
 header("Location: index.php"); 
 exit;
 ?>
